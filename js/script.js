a4b5880b31f6661f1f6731e6ed01ec4fb63cf4ca
Cristian Medina <medinacristian@hotmail.com>
2022-06-10 20:24:38 +0200

    Add hover effect that change bgcolor to black

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-const sketchArea = document.getElementById('sketchArea');
+const sketchArea = document.getElementById("sketchArea");
 for (let i = 0; i <= 16; i++) {
-    let squareRow = document.createElement('div');
-    squareRow.classList.add('flex-box'); 
-    sketchArea.appendChild(squareRow);
+  let squareRow = document.createElement("div");
+  squareRow.classList.add("flex-box");
+  sketchArea.appendChild(squareRow);
 
-    for (let j = 0; j <= 16; j++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        squareRow.appendChild(square);
-    }
+  for (let j = 0; j <= 16; j++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    squareRow.appendChild(square);
+  }
 }
+
+const divs = document.querySelectorAll(".square");
+
+function drawDiv(e) {
+    this.classList.add("bg-black");
+}
+
+divs.forEach((div) =>
+  div.addEventListener("mouseenter", drawDiv, {
+    capture: false,
+    once: true,
+  })
+);
