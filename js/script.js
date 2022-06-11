2fe30f04674b23bece1471b8f8c14595c39bc882
Cristian Medina <medinacristian@hotmail.com>
2022-06-11 19:22:38 +0200

    Remove unnecessary attributes on mouseenter func

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const sketchArea = document.getElementById("sketchArea");
+
+// Create divs for sketch area
 for (let i = 0; i <= 16; i++) {
   let squareRow = document.createElement("div");
   squareRow.classList.add("flex-box");
@@ -7,19 +9,17 @@ for (let i = 0; i <= 16; i++) {
   for (let j = 0; j <= 16; j++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.style.height = `${35 / 16}rem`;
+    square.style.width = `${35 / 16}rem`;
     squareRow.appendChild(square);
   }
 }
 
 const divs = document.querySelectorAll(".square");
 
-function drawDiv(e) {
-    this.classList.add("bg-black");
+// DrawDiv changes bg color on divs
+function drawDiv() {
+  this.classList.add("bg-black");
 }
 
-divs.forEach((div) =>
-  div.addEventListener("mouseenter", drawDiv, {
-    capture: false,
-    once: true,
-  })
-);
+divs.forEach((div) => div.addEventListener("mouseenter", drawDiv));
